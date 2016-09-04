62651bc0254e59af06d1fe8e8abd581b9db62dce
Dalton Menezes <daltonmenezes@outlook.com>
2016-09-04 13:04:42 -0300

    fix grammatical error about address word

--- src/basic_io2.c
+++ src/basic_io2.c
@@ -1,16 +1,14 @@
-#include <stdio.h>
-
 /*
 Exercise #2 from my college
 
 Make a program to read and write on console the following data:
-1.Name; 2.Adress; 3.City; 4.Zip code; 5. Phone number;
-
+1.Name; 2.Address; 3.City; 4.Zip code; 5. Phone number;
 */
+#include <stdio.h>
 
 int main() {
   char first_name[20];
-  char adress[100];
+  char address[100];
   char city[20];
   int zip_code;
   int phone_number;
@@ -18,8 +16,8 @@ int main() {
   printf("Type your first name: ");
   scanf("%s", first_name);
 
-  printf("Type your adress: ");
-  scanf(" %[^\n]", adress);
+  printf("Type your address: ");
+  scanf(" %[^\n]", address);
 
   printf("Type your city: ");
   scanf(" %[^\n]", city);
@@ -30,7 +28,7 @@ int main() {
   printf("Type your phone number: ");
   scanf ("%d", &phone_number);
 
-  printf("\nName: %s\nAdress: %s\nCity: %s\nZip code: %d\nPhone Number: %d\n", first_name, adress, city, zip_code, phone_number);
+  printf("\nName: %s\nAddress: %s\nCity: %s\nZip code: %d\nPhone Number: %d\n", first_name, address, city, zip_code, phone_number);
 
   return 0;
 }
