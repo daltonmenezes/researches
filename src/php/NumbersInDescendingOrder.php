c9609cd82e3d35d33087e31c8372d291314ce5c2
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-22 20:17:22 -0200

    add NumbersInDescendingOrder.php

---
+++ src/php/NumbersInDescendingOrder.php
@@ -0,0 +1,15 @@
+<?php
+
+$first_number = 0;
+$last_number = 10;
+
+$numbers = range($first_number, $last_number);
+rsort($numbers);
+
+foreach ($numbers as $index => $value) {
+  if ($index == $last_number) {
+    echo $value;
+  } else {
+    echo $value.' | ';
+  }
+}
