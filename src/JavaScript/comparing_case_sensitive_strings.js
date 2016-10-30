ab5879a83f957df090f0989c413d65ed707e7338
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-30 00:01:32 -0200

    Update comparing_case_sensitive_strings.js

--- src/JavaScript/comparing_case_sensitive_strings.js
+++ src/JavaScript/comparing_case_sensitive_strings.js
@@ -2,7 +2,7 @@ let text_one = 'HELLO WORLD';
 let text_two = 'hello world';
 
 if (text_one.toLowerCase() == text_two.toLowerCase())
-		console.log(text_one + ' and ' + text_two + ' has the same meaning.');
+    console.log(text_one + ' and ' + text_two + ' has the same meaning.');
 else
-		console.log(text_one + ' and ' + text_two + ' doesn\'t has the same meaning.');    
+    console.log(text_one + ' and ' + text_two + ' doesn\'t has the same meaning.');    
 
