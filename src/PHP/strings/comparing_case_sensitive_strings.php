5c4271ea0b9627e6fc704415f04eb747cb56bde2
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-29 23:59:50 -0200

    Update comparing_case_sensitive_strings.php

--- src/PHP/strings/comparing_case_sensitive_strings.php
+++ src/PHP/strings/comparing_case_sensitive_strings.php
@@ -4,8 +4,8 @@ $text_one = 'HELLO WORLD';
 $text_two = 'hello world';
 
 if (strtolower($text_one) == strtolower($text_two)) {
-		echo $text_one.' and '.$text_two.' has the same meaning.';
+    echo $text_one.' and '.$text_two.' has the same meaning.';
 }
 else {
-		echo $text_one.' and '.$text_two.' doesn\'t has the same meaning.';
+    echo $text_one.' and '.$text_two.' doesn\'t has the same meaning.';
 }
