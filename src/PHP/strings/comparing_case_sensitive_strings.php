44d3e43c4651118fb93874f18f8fadf397878e56
Dalton Menezes <daltonmenezes@outlook.com>
2016-10-29 23:58:29 -0200

    add comparing case sensitive strings file to PHP folder

---
+++ src/PHP/strings/comparing_case_sensitive_strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$text_one = 'HELLO WORLD';
+$text_two = 'hello world';
+
+if (strtolower($text_one) == strtolower($text_two)) {
+		echo $text_one.' and '.$text_two.' has the same meaning.';
+}
+else {
+		echo $text_one.' and '.$text_two.' doesn\'t has the same meaning.';
+}
