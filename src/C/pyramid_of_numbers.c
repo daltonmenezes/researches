e18a9a907e8da8504733212d67291958f31247c6
Dalton Menezes <daltonmenezes@outlook.com>
2016-11-12 23:33:35 -0200

    Add pyramid of numbers in C language

---
+++ src/C/pyramid_of_numbers.c
@@ -0,0 +1,31 @@
+/* Pyramid of numbers
+Exercise #7 from my college
+
+Make a program that's receive a number and print it like a pyramid in descending order.
+Example with number 5 inputted. The output must be like that:
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+*/
+#include <stdio.h>
+
+int main() {
+  int number;
+
+  printf("Type a number: ");
+  scanf("%d", &number);
+
+  for (int i = 1; i <= number; i++) {
+
+       for (int j = 1; j <= i; j++) {
+            printf ("%d ", j);
+       }
+
+       printf ("\n");
+  }
+
+  return 0;
+}
