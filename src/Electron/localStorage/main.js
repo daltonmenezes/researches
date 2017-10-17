c411fcd4c900ba6e240eebb060d8856cf697983b
Dalton Menezes <daltonmenezes@outlook.com>
2017-10-17 19:01:32 -0200

    Add localStorage test app in Electron folder

---
+++ src/Electron/localStorage/main.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const {app, BrowserWindow} = require('electron')
+
+let mainWindow
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    title: 'localStorage',
+    width: 400,
+    height: 250,
+    center: true,
+  })
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+    app.quit()
+  })
+})
+
+// Restore the window on Mac when the user click on the app icon in the dock
+app.on('activate', () => {
+  mainWindow.restore()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin')
+      app.quit()
+})
