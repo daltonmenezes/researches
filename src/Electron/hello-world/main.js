c072ed896596c68e93d99357be756953dc82479a
Dalton Menezes <daltonmenezes@outlook.com>
2019-08-08 03:24:56 -0300

    Refactor electron/hello-world

--- src/Electron/hello-world/main.js
+++ src/Electron/hello-world/main.js
@@ -1,31 +1,27 @@
-'use strict';
+const { app, BrowserWindow } = require('electron')
 
-const {app, BrowserWindow} = require('electron');
-
-let mainWindow;
+let mainWindow
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
     title: 'Hello World',
     width: 266,
     height: 150,
-    center: true,
-  });
+    center: true
+  })
 
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
-    app.quit();
-  });
-});
+  mainWindow.on('closed', () => app.quit())
+})
 
 // Restore the window on Mac when the user click on the app icon in the dock
 app.on('activate', () => {
-  mainWindow.restore();
-});
+  mainWindow.restore()
+})
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin')
-      app.quit();
-});
+  process.platform !== 'darwin'
+    ? app.quit()
+    : ''
+})
